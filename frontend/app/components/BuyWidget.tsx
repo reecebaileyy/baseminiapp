8350320b0c993b6456d3f1b0d89e9b9b431aae14
reecebaileyy <reece.bailey8857@yahoo.com>
2025-10-27 00:10:34 -0700

    update buy widget

--- frontend/app/components/BuyWidget.tsx
+++ frontend/app/components/BuyWidget.tsx
@@ -8,7 +8,7 @@ export function BuyWidget() {
 
   const ETHToken: Token = {
     name: "Ethereum",
-    address: "",
+    address: "0x0000000000000000000000000000000000000000",
     symbol: "ETH",
     decimals: 18,
     image: "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
